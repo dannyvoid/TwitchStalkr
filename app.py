b5dc75b5d63982bd7803881d74bb2cb1653acb1f
Danny Void <danielilluminatic@gmail.com>
2019-04-10 10:49:09 -0500

    0.6

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ client_id = config.get('default', 'client_id')
 stalking_interval = config.get('default', 'interval_in_seconds')
 streamer_name = config.get('default', 'streamer_name')
 
+token = config.get('telegram', 'token')
+chat_id = config.get('telegram', 'chat_id')
+
 if not streamer_name:
     streamer_name = input('Streamer to stalk: ')
 else:
@@ -33,6 +36,13 @@ def is_streaming(streamer_name):
     streamer_html = requests.get(twitch_api_stream_url)
     streamer = json.loads(streamer_html.content)
     return streamer['stream'] is not None
+
+
+def telegram(message):
+    send_message = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&parse_mode=Markdown&text={}'.format(
+        token, chat_id, message)
+    response = requests.get(send_message)
+    return response.json()
 
 
 def check_status():
@@ -64,6 +74,11 @@ def check_status():
 
         if not os.path.exists(temp_file):
             print('{} is online, opening in your default browser'.format(streamer_name))
+
+            if token and chat_id:
+                telegram(
+                    '[{}](https://twitch.tv/{}) is now streaming!'.format(streamer_name.capitalize(), streamer_name))
+
             start_time = time.time()
 
             with open(temp_file, 'w+') as timestamp:
@@ -86,7 +101,7 @@ def the_whole_thing():
 
     print('#########################')
     print('###   Twitch Stalkr   ###')
-    print('###       V 0.5       ###')
+    print('###       V 0.6       ###')
     print('#########################\n')
 
     check_status()
