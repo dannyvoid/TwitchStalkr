2a2d495ee89954ce598c9274b8c1bc02e54e33ee
Danny Void <danielilluminatic@gmail.com>
2019-04-14 00:37:27 -0500

    updated to v0.9
    - Changed from Advanced Python Scheduler to schedule.
    - Live countdown if the user is offline.
    - Sleep for 30 seconds when user first comes online. having an issue where it does this step twice if I don't.

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ import json
 import os
 import requests
 import time
+import schedule
 import webbrowser
 
-from apscheduler.schedulers.background import BackgroundScheduler
 from configparser import ConfigParser
 
+ver = '0.9'
+
 os.system('cls')
-os.system('title Twitch Stalkr v0.6')
+os.system('title Twitch Stalkr v {}'.format(ver))
 
 config = ConfigParser()
 config.read('config.ini')
 
 client_id = config.get('default', 'client_id')
-stalking_interval = config.get('default', 'interval_in_seconds')
+check_interval = int(config.get('default', 'interval_in_seconds'))
 streamer_name = config.get('default', 'streamer_name')
 
 token = config.get('telegram', 'token')
@@ -46,6 +48,8 @@ def telegram(message):
 
 
 def check_status():
+    check_interval = int(config.get('default', 'interval_in_seconds'))
+
     temp_file_dir = config.get('temp_file', 'directory')
     temp_file_suffix = config.get('temp_file', 'file_suffix')
     temp_file = os.path.join(temp_file_dir, '{}.{}'.format(
@@ -73,11 +77,11 @@ def check_status():
                 streamer_name.capitalize(), duration_hours, duration_minutes, duration_seconds))
 
         if not os.path.exists(temp_file):
-            print('{} is online, opening in your default browser'.format(streamer_name))
-
             if token and chat_id:
-                telegram(
-                    '[{}](https://twitch.tv/{}) is now streaming!'.format(streamer_name.capitalize(), streamer_name))
+                telegram('[{}]({}) is now streaming!'.format(
+                    streamer_name.capitalize(), streamer_url))
+
+            print('{} is online, opening in your default browser'.format(streamer_name))
 
             start_time = time.time()
 
@@ -86,14 +90,24 @@ def check_status():
 
             webbrowser.open_new_tab(streamer_url)
 
-            time.sleep(5)
+            time.sleep(30)
 
     if not is_streaming(streamer_name):
         if os.path.exists(temp_file):
             os.remove(temp_file)
 
         if not os.path.exists(temp_file):
-            print('{} is offline, checking again in 5 seconds'.format(streamer_name))
+            while (check_interval):
+                if check_interval > 1:
+                    print('{} is offline, checking again in {} seconds '.format(
+                        streamer_name, str(check_interval)), end='\r')
+                    check_interval -= 1
+                    time.sleep(1)
+                else:
+                    print('{} is offline, checking again in {} second '.format(
+                        streamer_name, str(check_interval)), end='\r')
+                    check_interval -= 1
+                    time.sleep(1)
 
 
 def the_whole_thing():
@@ -101,20 +115,14 @@ def the_whole_thing():
 
     print('#########################')
     print('###   Twitch Stalkr   ###')
-    print('###       V 0.6       ###')
+    print('###       V {}       ###'.format(ver))
     print('#########################\n')
 
     check_status()
 
 
-if __name__ == '__main__':
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(the_whole_thing, 'interval', seconds=int(
-        stalking_interval), misfire_grace_time=300, max_instances=10)
-    scheduler.start()
+schedule.every(0.5).seconds.do(the_whole_thing)
 
-    try:
-        while True:
-            time.sleep(5)
-    except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
+while True:
+    schedule.run_pending()
+time.sleep(1)
