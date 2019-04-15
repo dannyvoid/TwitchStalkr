640f1348da232bb7aaaa48572e222af9615f7018
Danny Void <danielilluminatic@gmail.com>
2019-04-15 14:26:57 -0500

    updated to 1.2
    - cleaned up output
    - moved to f-strings
    - check if you're even online to begin with

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 import requests
 import time
 import schedule
+import socket
 import webbrowser
 import cursor
 
@@ -11,138 +12,164 @@ from configparser import ConfigParser
 
 cursor.hide()
 
-ver = '1.0'
+appname = 'Twitch Stalkr'
+ver = '1.2'
 
-os.system('title Twitch Stalkr v {}'.format(ver))
+os.system(f'title {appname} v{ver}')
 
 config = ConfigParser()
 config.read('config.ini')
 
 client_id = config.get('default', 'client_id')
-check_interval = int(config.get('default', 'interval_in_seconds'))
+check_int = int(config.get('default', 'int_in_seconds'))
 streamer_name = config.get('default', 'streamer_name')
 
 token = config.get('telegram', 'token')
 chat_id = config.get('telegram', 'chat_id')
 
 if not streamer_name:
-    streamer_name = input('Streamer to stalk: ')
+    streamer_name = input('Streamer to stalk: ').capitalize()
 else:
-    streamer_name = streamer_name
+    streamer_name = streamer_name.capitalize()
 
-streamer_url = 'https://twitch.tv/{}'.format(streamer_name)
+streamer_url = f'https://twitch.tv/{streamer_name.lower()}'
 
-os.system('title Stalking {}'.format(streamer_name.capitalize()))
+os.system(f'title Stalking {streamer_name}')
 
 
-def is_streaming(streamer_name):
-    twitch_api_stream_url = 'https://api.twitch.tv/kraken/streams/{}?client_id={}'.format(
-        streamer_name, client_id)
+def online():
+    try:
+        socket.create_connection(('www.google.com', 80))
+        return True
+    except Exception:
+        pass
+    return False
+
+
+def broadcasting(streamer_name):
+    twitch_api_stream_url = f'https://api.twitch.tv/kraken/streams/{streamer_name.lower()}?client_id={client_id}'
     streamer_html = requests.get(twitch_api_stream_url)
     streamer = json.loads(streamer_html.content)
     return streamer['stream'] is not None
 
 
 def telegram(message):
-    send_message = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&parse_mode=Markdown&text={}'.format(
-        token, chat_id, message)
+    send_message = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={message}'
     response = requests.get(send_message)
     return response.json()
 
 
-def check_status():
-    check_interval = config.get('default', 'interval_in_seconds')
-    check_interval = int(check_interval)
-
-    double_protection = config.get('default', 'double_protection')
-    double_protection = int(double_protection)
-
+def status():
     temp_file_dir = config.get('temp_file', 'directory')
     temp_file_suffix = config.get('temp_file', 'file_suffix')
-    temp_file = os.path.join(temp_file_dir, '{}.{}'.format(
-        streamer_name, temp_file_suffix))
+    temp_file = os.path.join(temp_file_dir, f'{streamer_name.lower()}.{temp_file_suffix}')
 
-    if is_streaming(streamer_name):
+    if broadcasting(streamer_name):
         if os.path.exists(temp_file):
+            with open(temp_file, 'r') as start_t:
+                start_t = start_t.readline()
 
-            with open(temp_file, 'r') as start_time:
-                start_time = start_time.readline()
+            start_t = float(start_t)
+            current_t = time.time()
 
-            start_time = float(start_time)
-            current_time = time.time()
+            dur = current_t - start_t
+            dur = int(dur)
 
-            stream_duration = current_time - start_time
-            stream_duration = int(stream_duration)
+            hrs = dur // 3600
+            mins = (dur - (hrs * 3600)) // 60
+            secs = dur - ((hrs * 3600) + (mins * 60))
 
-            duration_hours = stream_duration // 3600
-            duration_minutes = (
-                stream_duration - (duration_hours * 3600)) // 60
-            duration_seconds = stream_duration - \
-                ((duration_hours * 3600) + (duration_minutes * 60))
+            if secs == 1:
+                print(
+                    f'{streamer_name} has been streaming for: '
+                    f'{hrs} hours, {mins} minutes, and '
+                    f'{secs} second.', end='\033[K\r', flush=True
+                )
 
-            print('{} has been streaming for: {} hours, {} minutes, and {} seconds.'.format(
-                streamer_name.capitalize(), duration_hours, duration_minutes, duration_seconds))
-
-            sys.stdout.write("\033[F")
-            sys.stdout.write("\033[K")
+            else:
+                print(
+                    f'{streamer_name} has been streaming for: '
+                    f'{hrs} hours, {mins} minutes, and '
+                    f'{secs} seconds.', end='\033[K\r', flush=True
+                )
 
         if not os.path.exists(temp_file):
             if token and chat_id:
-                telegram('[{}]({}) is now streaming!'.format(
-                    streamer_name.capitalize(), streamer_url))
+                telegram(f'[{streamer_name}]({streamer_url}) is now streaming!')
 
-            print('{} is online, opening in your default browser.'.format(
-                streamer_name.capitalize()))
-
+            print(f'{streamer_name} is online, opening in your default browser.', end='\033[K\r', flush=True)
             time.sleep(5)
 
-            sys.stdout.write("\033[F")
-            sys.stdout.write("\033[K")
-
-            start_time = time.time()
+            start_t = time.time()
 
             with open(temp_file, 'w+') as timestamp:
-                timestamp.write(str(start_time))
+                timestamp.write(str(start_t))
 
             webbrowser.open_new_tab(streamer_url)
 
-            while (double_protection):
+            double_protection = int(config.get('default', 'double_protection'))
+
+            while double_protection:
                 if double_protection > 1:
-                    print('{} is online, waiting for {} seconds. '.format(
-                        streamer_name.capitalize(), str(double_protection)), end='\r')
-                    double_protection -= 1
-                    time.sleep(1)
-                else:
-                    print('{} is online, waiting for {} second. '.format(
-                        streamer_name.capitalize(), str(double_protection)), end='\r')
+                    print(f'{streamer_name} is online, waiting for {double_protection} seconds.', end='\033[K\r', flush=True)
                     double_protection -= 1
                     time.sleep(1)
 
-    if not is_streaming(streamer_name):
+                else:
+                    print(f'{streamer_name} is online, waiting for {double_protection} second.', end='\033[K\r', flush=True)
+                    double_protection -= 1
+                    time.sleep(1)
+
+    if not broadcasting(streamer_name):
         if os.path.exists(temp_file):
             os.remove(temp_file)
 
         if not os.path.exists(temp_file):
-            while (check_interval):
-                if check_interval > 1:
-                    print('{} is offline, checking again in {} seconds. '.format(
-                        streamer_name.capitalize(), str(check_interval)), end='\r')
-                    check_interval -= 1
+            check_int = int(config.get('default', 'int_in_seconds'))
+
+            while check_int:
+                if check_int > 1:
+                    print(f'{streamer_name} is offline, checking again in {check_int} seconds.', end='\033[K\r', flush=True)
+                    check_int -= 1
                     time.sleep(1)
+
                 else:
-                    print('{} is offline, checking again in {} second. '.format(
-                        streamer_name.capitalize(), str(check_interval)), end='\r')
-                    check_interval -= 1
+                    print(f'{streamer_name} is offline, checking again in {check_int} second.', end='\033[K\r', flush=True)
+                    check_int -= 1
                     time.sleep(1)
 
 
-print('#########################')
-print('###   Twitch Stalkr   ###')
-print('###       V {}       ###'.format(ver))
-print('#########################\n')
+def lets_do_this():
+    if online():
+        try:
+            status()
+        except Exception:
+            pass
+    else:
+        check_int = int(config.get('default', 'int_in_seconds'))
 
-schedule.every(0.5).seconds.do(check_status)
+        while check_int:
+            if check_int > 1:
+                print(f'You are not connected to the internet. Trying again in {check_int} seconds. ', end='\033[K\r', flush=True)
+                check_int -= 1
+                time.sleep(1)
+
+            else:
+                print(f'You are not connected to the internet. Trying again in {check_int} second.', end='\033[K\r', flush=True)
+                check_int -= 1
+                time.sleep(1)
+
+
+print(
+    f'#####################\n'
+    f'#{appname:^19}#\n'
+    f'#{ver:^19}#\n'
+    f'#####################\n'
+)
+
+schedule.every(0.1).seconds.do(lets_do_this)
 
 while True:
     schedule.run_pending()
-time.sleep(0.7)
+
+time.sleep(0.1)
